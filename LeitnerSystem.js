7b9c03562da379642cbe3d1eb0cbc19e48e4fb9f
etgertz <etgertz@gmail.com>
2025-03-04 21:48:38 -0600

    Added activeDeck and use functions.

--- LeitnerSystem.js
+++ LeitnerSystem.js
@@ -2,6 +2,8 @@
 import { Deck } from "./Deck.js";
 
 export class LeitnerSystem {
+  static activeDeck=null;
+
   constructor() {
     this.decks = {};
   }
@@ -18,6 +20,14 @@ export class LeitnerSystem {
 
   listDecks() {
     return Object.keys(this.decks);
+  }
+
+  setActiveDeck(name){
+    this.activeDeck = getDeck(name);
+  }
+
+  getActiveDeck(){
+    return this.activeDeck;
   }
 }
 //module.exports = LeitnerSystem;
